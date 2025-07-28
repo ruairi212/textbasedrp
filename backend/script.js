b486da122226c56a5154fd4fa2e568ab7512b540
ruairi <ruairitierney212@gmail.com>
2025-07-28 23:25:07 +0100

    small changes

--- backend/script.js
+++ backend/script.js
@@ -1,21 +1,9 @@
 let xp = 0;
-let health = 100;
-let gold = 50;
-let current_Weapon = 0;
-let fighting ;
-let monsterHealth;
-let inventory = ["stick"];
+
 
 const button1 = document.querySelector("#buttotn1");
 const button2 = document.querySelector("#buttotn2");
-const button3 = document.querySelector("#buttotn3");
-const text = document.querySelector("#text");
-const xpText = document.querySelector("#xpText");
-const healthText = document.querySelector("#healthText");
-const goldText = document.querySelector("#goldText");
-const monsterStats = document.querySelector("#monsterStats");
-const monsterNameText = document.querySelector("#monsterNameText");
-const monsterHealthText = document.querySelector("#monsterHealthText");
+
 
 // initialise the buttons
 
