b1b28e9caa942a6ffa7c19ca13507666c0b32309
ruairi <ruairitierney212@gmail.com>
2025-08-01 09:00:48 +0100

    basic form validation

--- backend/script.js
+++ backend/script.js
@@ -1,16 +1,29 @@
-let xp = 0;
 
-
-const button1 = document.querySelector("#buttotn1");
-const button2 = document.querySelector("#buttotn2");
-
-
-// initialise the buttons
-
-button1.onclick = goStore;
-button2.onclick = goCave
-button3.onclick = fightBoss
-
-function goStore() {
-    console.log("Going to the store.")
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password= document.getElementById('password');
+const password2= document.getElementById('password2');
+function showSuccess(){
+    
 }
+function showError(){
+
+}
+
+function checkRequired(inputArr){
+    inputArr.forEach(function(input){
+        if(input.value.trim() === '') {
+            showError(input,`${input.id} is required`);
+        }else{
+            showSuccess(input)
+        }
+    });
+}
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+
+    checkRequired([username,email,password,password2]);
+});
+
